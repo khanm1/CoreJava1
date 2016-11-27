098c1a209f164228a348f27e89da4d3494f67382
mkhan <khanmohammad269@gmail.com>
2016-11-27 10:40:42 -0600

    initial setup

---
+++ Documents/workspace/Project1/src/classNobject/House.java
@@ -0,0 +1,27 @@
+package classNobject;
+
+public class House {
+
+	//Variables
+		public int adress = 1625;
+		public String className;
+		
+		//Method
+		public void watchTVShows () {
+			System.out.println("Watching collage football game");
+			
+		}
+		public void sleep () {
+			System.out.println("Watching collage football game");
+			
+		}
+		
+		public int addHouseSize (int leanth, int height) {
+			int total = leanth + height;
+			return total;
+					
+		}
+		
+	}
+
+
